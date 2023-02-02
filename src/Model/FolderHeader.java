ff39706e7fae4626210a72c1db0bd95f7149c633
Vilmos Bognar <realvilmos@gmail.com>
2023-02-02 08:13:16 +0100

    A fejlécek fájlba írása

--- src/Model/FolderHeader.java
+++ src/Model/FolderHeader.java
@@ -41,5 +41,48 @@ public class FolderHeader {
         this.creationDate = creationDate;
     }
     
+    public String getIndicatedStringBinaryId(){
+        String binary = Integer.toBinaryString(this.id);
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(binary);
+        sb.reverse();
+        
+        String s[] = sb.toString().split("(?<=\\G.{7})");
+        String indicatedBinaryString = "";
+        
+        for(int i = 0 ; i < s.length; i++){
+            sb.setLength(0);
+            sb.append(s[i]);
+            s[i] = sb.reverse().toString();
+        }
+        
+        while(s[s.length-1].length() % 7 != 0){
+            s[s.length-1] = "0" + s[s.length-1];
+        }
+        
+        for(int i = s.length-1; i >= 0; i--){
+            if(i > 0){
+                indicatedBinaryString += ("1"+s[i]);
+            }else{
+                indicatedBinaryString += ("0"+s[i]);
+            }
+        }
+        
+        System.out.println(indicatedBinaryString);
+        
+        return indicatedBinaryString;
+    }
     
+    public void setIdFromBinaryIndicatedString(String binaryIndicatedString){
+        String binaryString = "";
+        
+        String s[] = binaryIndicatedString.split("(?<=\\G.{8})");
+        
+        for(int i = 0; i < s.length; i++){
+            binaryString += s[i].substring(1, 8);
+        }
+
+        this.id = Integer.parseInt(binaryString, 2);
+    }
 }
--- src/Services/CompressService.java
+++ src/Services/CompressService.java
@@ -7,13 +7,14 @@ import Model.HierarchyInterface;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public abstract class CompressService {
     
     //Mivel a header megoldások bármely jővőbeli tömörítési eljárásban ugyanazon az elven működnének, itt definiálom őket.
     
     private int id = 0;
-    private ArrayList<FileHeader> definedHeaders = new ArrayList<>();
+    private ArrayList<FolderHeader> definedHeaders = new ArrayList<>();
     private Folder rootFolder;
     
     CompressService(Folder folder){
@@ -24,14 +25,17 @@ public abstract class CompressService {
         initializeHeaders(this.rootFolder);
  
         try {
-            PrintWriter pw = new PrintWriter("Merged.txt");
+            PrintWriter pw = new PrintWriter("Compressed.txt");
             compressElementsInFolder(this.rootFolder, pw);
             
             //ide jöhet a 6 bájtos elválasztó "pecsét"
-            //Bognár Vilmos 6 Bájtos Pecsétje :)
-            pw.write("BV6BP");
-            //valami makeHeader/writeHeader ami bele is írja már a fájlba ÉS hozzáadja a távolságot a többi headerhöz
-
+            //Bognár Vilmos Zsolt 6 Bájtos Pecsétje :)
+            String sixByteSignature = "BVZ6BP";
+            pw.write(sixByteSignature);
+            addDistanceToHeaders((long)sixByteSignature.length());
+            //valami makeHeader
+            writeHeaders(pw);
+            
             //Hierarchia ábrázolása stringben, ezt esetleg lehet kombinálni a writeHeaderrel hogy közbe történjen
             
             pw.flush();
@@ -57,8 +61,9 @@ public abstract class CompressService {
     protected void compressElementsInFolder(Folder folder, PrintWriter pw){
         for(HierarchyInterface Elem : folder.getChildren()){
            if(Elem instanceof Folder){
-                //Itt is csinálni Headert
                 compressElementsInFolder((Folder)Elem, pw);
+                FolderHeader header = Elem.getHeader();
+                addDefinedHeaderToList(header);
                 
             }else{
                 File f = (File)Elem;
@@ -81,13 +86,15 @@ public abstract class CompressService {
         return new long[]{0, 0};
     }
     
-    protected void addDefinedHeaderToList(FileHeader header){
+    protected void addDefinedHeaderToList(FolderHeader header){
         definedHeaders.add(header);
     }
     
     protected void addDistanceToHeaders(Long distance){
-        for(FileHeader h : definedHeaders){
-            h.addDistanceFromHeader(distance);
+        for(FolderHeader h : definedHeaders){
+            if(h instanceof FileHeader fileHeader){
+                fileHeader.addDistanceFromHeader(distance);
+            }
         }
     }
     
@@ -95,6 +102,98 @@ public abstract class CompressService {
         this.id = this.id + 1;
         return this.id;
     }
+    
+    //Defined header-ön kéne végig loopolni még mindig
+    private void writeHeaders(PrintWriter pw){
+        
+        for(int i = 0; i < definedHeaders.size(); i++){
+            int bytesUsedForHeader = 0;
+            if(definedHeaders.get(i) instanceof FileHeader fileHeader){
+                //id
+                String indicatedBinaryId = fileHeader.getIndicatedStringBinaryId();
+                bytesUsedForHeader += writeBinaryToFile(indicatedBinaryId, pw);
+
+                //relatív elhelyezkedés
+                bytesUsedForHeader += writeSizeAndBinaryToFile(fileHeader.getDistanceFromHeader(), pw);
+
+                //Tömörített állomány mérete
+                bytesUsedForHeader += writeSizeAndBinaryToFile(fileHeader.getFileSize(), pw);
+
+                //szemét bitek
+                bytesUsedForHeader += writeBinaryToFile(numberToBinary(fileHeader.getJunkBits()), pw);
+
+                //Név és kiterjesztés
+                bytesUsedForHeader += writeBinaryToFile(numberToBinary(fileHeader.getNameAndExtension().length()), pw);
+                pw.append(fileHeader.getNameAndExtension());
+                bytesUsedForHeader += fileHeader.getNameAndExtension().length();
+
+                //Dátumok
+                Calendar calendar = Calendar.getInstance();
+
+                //Módosítás dátuma
+                calendar.setTime(fileHeader.getModificationDate());
+                bytesUsedForHeader += writeSizeAndBinaryToFile(calendar.get(Calendar.SECOND), pw);
+
+                //Létrehozás dátuma
+                calendar.setTime(fileHeader.getCreationDate());
+                bytesUsedForHeader += writeSizeAndBinaryToFile(calendar.get(Calendar.SECOND), pw);
+            }else{
+                //azonosító
+                String indicatedBinaryId = definedHeaders.get(i).getIndicatedStringBinaryId();
+                bytesUsedForHeader += writeBinaryToFile(indicatedBinaryId, pw);
+                
+                //Név
+                bytesUsedForHeader += writeBinaryToFile(numberToBinary(definedHeaders.get(i).getNameAndExtension().length()), pw);
+                pw.append(definedHeaders.get(i).getNameAndExtension());
+                bytesUsedForHeader += definedHeaders.get(i).getNameAndExtension().length();
+                
+                //Létrehozás dátuma
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(definedHeaders.get(i).getCreationDate());
+                bytesUsedForHeader += writeSizeAndBinaryToFile(calendar.get(Calendar.SECOND), pw);
+                
+                writeHeaders(pw);
+            }
+            
+            //Header távolság hozzáadása a következő headerökhöz
+            
+            for(int j = i; j < definedHeaders.size(); j++){
+                if(definedHeaders.get(j) instanceof FileHeader fileHeader){
+                    fileHeader.addDistanceFromHeader(bytesUsedForHeader);
+                }
+            }
+           
+        }
+    }
+    
+    private int writeSizeAndBinaryToFile(long num, PrintWriter pw){
+        int bytesUsed = 0;
+        String s = numberToBinary(num);
+        bytesUsed += writeBinaryToFile(numberToBinary(s.length()/8), pw);
+        bytesUsed += writeBinaryToFile(s, pw);
+        return bytesUsed;
+    }
+    
+       
+    private String numberToBinary(long num){
+        String binary = Long.toBinaryString(num);
+        while(binary.length() % 8 != 0){
+            binary = "0" + binary;
+        }
+        
+        return binary;
+    }
+    
+    private int writeBinaryToFile(String binary, PrintWriter pw){
+        String s[] = binary.split("(?<=\\G.{8})");
+               
+        for(int i = 0; i < s.length; i++){
+            pw.append((char)Integer.parseInt(s[i], 2));
+        }
+        
+        return s.length;
+    }
+    
     
     
 }
